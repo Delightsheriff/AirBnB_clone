ab3ffc616e028a5b5c69402f547478783425045a
beekaz <sooperfect00@gmail.com>
2023-08-14 02:11:08 +0000

    class

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This file defines the State class,
+which represents a state in the system.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class
+
+    Attributes:
+        name (str): The state's name.
+    """
+    name = " "
