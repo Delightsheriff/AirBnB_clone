499bfb2f4a2d457eb9c5e49526af6b034e2c1d2a
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-08-26 01:33:14 +0000

    Updated reviews

--- models/review.py
+++ models/review.py
@@ -15,6 +15,15 @@ class Review(BaseModel):
         user_id (str): The ID of the user who wrote the review.
         text (str): The text of the review.
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            Initialize clss user with kwargs
+            Args:
+                *args(positional arg): strings
+                **kwargs(keyword arg): dictionary
+        """
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
