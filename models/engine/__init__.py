3c9555ac0902d9d04597f5bbf5d89c1e772974ce
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-08-26 02:03:18 +0000

    Update init model for py storage

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""__init__ magic method for models directory"""
+
+from models.engine.file_storage import FileStorage
+"""Initialize the package"""
+
+storage = FileStorage()
+storage.reload()
