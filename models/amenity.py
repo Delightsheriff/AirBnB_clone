6eadb8bee0ac7d0ae4791c274cc3fc829b3cccb4
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-08-26 01:10:58 +0000

    Updated amenities

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,6 @@ This file defines the Amenity class,
 which represents an amenity in the system.
 """
 
-
 from models.base_model import BaseModel
 
 
@@ -15,4 +14,12 @@ class Amenity(BaseModel):
     Attributes:
         name (str): The name of the amenity.
     """
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """
+            Initialize clss user with kwargs
+            Args:
+                *args(positional arg): strings
+                **kwargs(keyword arg): dictionary
+        """
+        super().__init__(*args, **kwargs)
+        self.name = ""
